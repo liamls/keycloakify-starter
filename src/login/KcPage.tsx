4fa6a04728ebf6806ed936b15cd4d99feae74c94
Liam Le Strat <lestratliam@gmail.com>
2025-12-03 16:49:05 +0100

    init with login and register screen

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -3,10 +3,13 @@ import type { ClassKey } from "keycloakify/login";
 import type { KcContext } from "./KcContext";
 import { useI18n } from "./i18n";
 import DefaultPage from "keycloakify/login/DefaultPage";
-import Template from "keycloakify/login/Template";
-const UserProfileFormFields = lazy(
-    () => import("keycloakify/login/UserProfileFormFields")
-);
+import Template from "./Template";
+import "./main.css";
+
+const UserProfileFormFields = lazy(() => import("./UserProfileFormFields"));
+
+const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
 
 const doMakeUserConfirmPassword = true;
 
@@ -19,6 +22,24 @@ export default function KcPage(props: { kcContext: KcContext }) {
         <Suspense>
             {(() => {
                 switch (kcContext.pageId) {
+                    case "login.ftl":
+                        return (
+                            <Login
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                            />
+                        );
+                    case "register.ftl":
+                        return (
+                            <Register
+                                {...{ kcContext, i18n, classes }}
+                                Template={Template}
+                                doUseDefaultCss={true}
+                                UserProfileFormFields={UserProfileFormFields}
+                                doMakeUserConfirmPassword={doMakeUserConfirmPassword}
+                            />
+                        );
                     default:
                         return (
                             <DefaultPage
--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,50 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withCustomTranslations({
+        en: {
+            newsletterInfo: "What we will send you:",
+            newsletterInfoCheckbox1: "News about the FUN platform",
+            newsletterInfoCheckbox2:
+                "Information about the courses you want to follow or are following",
+            subscribeNewsletter: "Discover the newsletters you can subscribe to by",
+            newsletter: "clicking here.",
+            newsletterUrl: "https://www.fun-mooc.fr/en/actualites/infolettres/",
+            contactSupport: "If you face any issues,",
+            zendesk: "contact the support.",
+            zendeskUrl: "https://www.fun-mooc.help/hc/en-gb/requests/new",
+            warningActivation:
+                "The account database is being updated. If you already have a FUN account, you must reactivate it to keep it. Without reactivation, your account will be deactivated. If you have already completed this reactivation between XXXX and XXXX, you can ignore this message.",
+            optionnalField: "optional",
+            passwordInstruction1: "At least 14 characters",
+            passwordInstruction2: "At least one uppercase letter",
+            passwordInstruction3: "At least one lowercase letter",
+            passwordInstruction4: "At least one digit"
+        },
+        fr: {
+            newsletterInfo: "Ce que nous vous enverrons :",
+            newsletterInfoCheckbox1: "Des nouvelles concernant la plateforme FUN",
+            newsletterInfoCheckbox2:
+                "Les informations concernant les cours que vous voulez suivre ou que vous suivez",
+            subscribeNewsletter:
+                "Découvrez les infolettres auxquelles vous pouvez vous inscrire en",
+            newsletter: "cliquant ici.",
+            newsletterUrl: "https://www.fun-mooc.fr/fr/actualites/infolettres/",
+            contactSupport: "En cas de problème,",
+            zendesk: "contactez l’assistance technique.",
+            zendeskUrl: "https://www.fun-mooc.help/hc/fr/requests/new",
+            warningActivation:
+                "La base des comptes évolue. Si vous possédez déjà un compte FUN, vous devez le réactiver pour le conserver. Sans réactivation, votre compte sera désactivé. Si vous avez déjà procédé à cette réactivation entre le XXXX et le XXXX, vous pouvez ignorer ce message.",
+            optionnalField: "optionnel",
+            passwordInstruction1: "Au moins 14 caractères",
+            passwordInstruction2: "Au moins une lettre majuscule",
+            passwordInstruction3: "Au moins une lettre minuscule",
+            passwordInstruction4: "Au moins un chiffre"
+        }
+    })
+    .build();
 
 type I18n = typeof ofTypeI18n;
 
